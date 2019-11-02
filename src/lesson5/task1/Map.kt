a73a2ffc448825286f643ad2770148a9a83d197c
Mad-User <kotov.andrey01@mail.ru>
2019-11-02 16:40:01 +0300

    canBuildFrom done

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -241,7 +241,14 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean = TODO()
+fun canBuildFrom(chars: List<Char>, word: String): Boolean {
+    if (chars.isEmpty()) return false
+
+    for (char in chars)
+        if (!word.contains(char)) return false
+
+    return true
+}
 
 /**
  * Средняя
